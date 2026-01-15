e88962f485338e63eeed8994c8ec9bdde0ed28d5
raka-wav <rakanewdevice0@gmail.com>
2026-01-15 15:20:07 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+const brain = {
+  hello: "Hello artinya halo. Contoh: Hello, how are you?",
+  cat: "Cat artinya kucing.",
+  love: "Love artinya cinta."
+};
+
+function ask() {
+  const input = document.getElementById("userInput");
+  const text = input.value.toLowerCase();
+  input.value = "";
+
+  addChat("user", text);
+
+  let answer = "Maaf, aku belum ngerti 😅";
+
+  for (let key in brain) {
+    if (text.includes(key)) {
+      answer = brain[key];
+    }
+  }
+
+  setTimeout(() => {
+    addChat("bot", answer);
+  }, 500);
+}
+
+function addChat(type, text) {
+  const chat = document.getElementById("chat");
+  const div = document.createElement("div");
+  div.className = type;
+  div.innerText = text;
+  chat.appendChild(div);
+  chat.scrollTop = chat.scrollHeight;
+}
